070b96f0793b165dc90f2c3d1205add1189c81c2
Justin Welsh <justinwelsh33@gmail.com>
2023-07-19 15:57:18 -0500

    made recipe-item its own component

--- recipe-book-app/src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-book-app/src/app/components/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe';
 
 @Component({
   selector: 'app-recipe-item',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class RecipeItemComponent {
 
+  @Input() recipe!: Recipe;
 }
